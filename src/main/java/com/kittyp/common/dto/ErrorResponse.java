0ce008822d52a3a1f98b005701c086fd40258ceb
Rohan <rrohn419@gmail.com>
2025-04-21 19:13:53 +0530

    handled global exception handler method

--- src/main/java/com/kittyp/common/dto/ErrorResponse.java
+++ src/main/java/com/kittyp/common/dto/ErrorResponse.java
@@ -4,9 +4,9 @@
 package com.kittyp.common.dto;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -22,7 +22,6 @@ public class ErrorResponse<T> {
 	private boolean success;
 	private String message;
 	private String detailedMessage;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime timestamp;
 	private Integer status;
 	private List<T> errors;
