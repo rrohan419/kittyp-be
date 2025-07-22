244ba21d69a8fb24b4c2530861ef41b42a1660fe
Rohan <rrohan419@gmail.com>
2025-07-22 11:28:05 +0530

    Update AuthEntryPointJwt.java
    added PATCG in allowed headers

--- src/main/java/com/kittyp/auth/util/AuthEntryPointJwt.java
+++ src/main/java/com/kittyp/auth/util/AuthEntryPointJwt.java
@@ -38,7 +38,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         response.setHeader("Access-Control-Allow-Origin", "*"); // or specific domain
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATVH, DELETE, OPTIONS");
 
         
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
