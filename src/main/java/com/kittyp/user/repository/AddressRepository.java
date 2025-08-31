65805ddf1444373e14dd2e540472484f520d0773
rohan.shrivastava <rohan.shrivastava@mindbowser.com>
2025-08-31 07:49:51 +0530

    added sort by created at in get addresses

--- src/main/java/com/kittyp/user/repository/AddressRepository.java
+++ src/main/java/com/kittyp/user/repository/AddressRepository.java
@@ -2,6 +2,7 @@ package com.kittyp.user.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kittyp.user.entity.Address;
@@ -11,5 +12,5 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
     Address findByUuid(String uuid);
     void deleteByUuid(String uuid);
 
-    List<Address> findByUserUuid(String userUuid);
+    List<Address> findByUserUuid(String userUuid, Sort sort);
 }
