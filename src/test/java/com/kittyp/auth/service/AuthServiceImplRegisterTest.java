8979d0540f66a06f613afc37709d9b454942c6ae
Rohan <rrohan419@gmail.com>
2026-09-22 20:46:39 +0530

    Refactor AuthServiceImpl instantiation in AuthServiceImplRegisterTest

--- src/test/java/com/kittyp/auth/service/AuthServiceImplRegisterTest.java
+++ src/test/java/com/kittyp/auth/service/AuthServiceImplRegisterTest.java
@@ -77,22 +77,22 @@ class AuthServiceImplRegisterTest {
 		when(clinicDao.saveClinic(any(Clinic.class))).thenAnswer(invocation -> invocation.getArgument(0));
 		when(clinicDao.findAllByOwnerUserId(any())).thenReturn(List.of());
 
-		// authService = new AuthServiceImpl(
-		// 		userDao,
-		// 		encoder,
-		// 		roleDao,
-		// 		null,
-		// 		null,
-		// 		zeptoMailService,
-		// 		null,
-		// 		clinicDao,
-		// 		clinicDoctorRepository,
-		// 		clinicDoctorInviteRepository,
-		// 		doctorProfileDao,
-		// 		verificationCodeService,
-		// 		null,
-		// 		clinicOwnerUserLinkService,
-		// 		null);
+		authService = new AuthServiceImpl(
+				userDao,
+				encoder,
+				roleDao,
+				null,
+				null,
+				zeptoMailService,
+				null,
+				clinicDao,
+				clinicDoctorRepository,
+				clinicDoctorInviteRepository,
+				doctorProfileDao,
+				verificationCodeService,
+				null,
+				clinicOwnerUserLinkService,
+				null);
 	}
 
 	@Test
