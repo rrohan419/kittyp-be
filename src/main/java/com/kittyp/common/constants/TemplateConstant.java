32ffed5b746381e83f3ec37f470278562f17f5af
Rohan <rrohan419@gmail.com>
2026-09-22 19:09:35 +0530

    corrected the template name for reset password

--- src/main/java/com/kittyp/common/constants/TemplateConstant.java
+++ src/main/java/com/kittyp/common/constants/TemplateConstant.java
@@ -20,7 +20,7 @@ public class TemplateConstant {
 	public static final String ZEPTO_CLINIC_DOCTOR_INVITE_EMAIL_TEMPLATE_ID = "zoho.clinic.doctor.invite.email.template.id";
 	public static final String ZEPTO_CLINIC_STAFF_INVITE_EMAIL_TEMPLATE_ID = "zoho.clinic.staff.invite.email.template.id";
 	public static final String ZEPTO_CLINIC_DOCTOR_INVITE_RESPONSE_EMAIL_TEMPLATE_ID = "zoho.clinic.doctor.invite.response.email.template.id";
-	public static final String ZEPTO_RESET_PASSWORD_CODE_EMAIL_TEMPLATE_ID = "password.reset.code.email.template.id";
-	public static final String ZEPTO_ORDER_CONFIRMATION_EMAIL_TEMPLATE_ID = "order.confirmation.email.template.id";
+	public static final String ZEPTO_RESET_PASSWORD_CODE_EMAIL_TEMPLATE_ID = "zoho.password.reset.code.email.template.id";
+	public static final String ZEPTO_ORDER_CONFIRMATION_EMAIL_TEMPLATE_ID = "zoho.order.confirmation.email.template.id";
 
 }
